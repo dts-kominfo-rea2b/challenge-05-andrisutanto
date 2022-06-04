285bc5a2496e6206e02843aa26b5de6344048eab
Andri Sutanto <andrisutantochen@gmail.com>
2022-06-04 21:29:45 +0700

    Submit Tugas

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, sortType) => {
+    const sortedData = sortType(data);
+    const sortedNames = [];
+    for (let i = 0; i < sortedData.length; i++) {
+        sortedNames[i] = `${i + 1}. ${sortedData[i]}`;
+    }
+    return sortedNames;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+    return data.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+    return data.reverse();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
